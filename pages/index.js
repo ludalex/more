0035fabdda7a91c3df23a4e277346f69fa334509
ludalex <ludalex@gmail.com>
2019-09-19 16:45:17 +0200

    enc and trimmed video. autoplay

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import ReactPlayer from 'react-player'
 import Main from '../layouts/main'
-import './index.scss';
 
 const videoWrapperStyle = { "position": "fixed", "top": "0", "right": "0", "bottom": "0", "left": "0", "overflow": "hidden", "zIndex": "-100" }
 const videoStyle = { "position": "absolute", "top": "50%", "left": "50%", "width": "auto", "height": "auto", "minWidth": "100%", "minHeight": "100%", "WebkitTransform": "translate(-50%, -50%)", "MozTransform": "translate(-50%, -50%)", "MsTransform": "translate(-50%, -50%)", "transform": "translate(-50%, -50%)" }
@@ -8,7 +7,7 @@ const contentStyle = { "margin": "0 auto", "width": "100%", "height": "100%", "m
 
 const Index = () => (
     <Main>
-      <ReactPlayer url='/static/intro1.mp4' loop muted autplay="true" playsinline width='100%' height='100%' style={videoWrapperStyle} config={{
+      <ReactPlayer url='/static/intro1_enc_trim.mp4' loop muted playing={true} playsinline width='100%' height='100%' style={videoWrapperStyle} config={{
         file: {
           attributes: {
             style: videoStyle
